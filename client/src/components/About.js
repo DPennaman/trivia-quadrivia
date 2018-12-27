e909c91b2f80ccb88f6f9a39b33318410f1d3228
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2018-12-27 11:44:20 -0500

    added headshots starting on about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,1 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render(){
+        return (<h1>About Page</h1>);
+    }
+}
+
+export default About
