3dffa6a6efa84436a97e759107d4c6b457b608a0
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2018-12-28 10:01:39 -0500

    added home changed app added functional about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 
-class About extends Component {
-    render(){
-        return (<h1>About Page</h1>);
-    }
-}
+const About = () => (
+      <div className='about'>
+        <h1>About Me</h1>
+        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+      </div>
+    );
 
 export default About
