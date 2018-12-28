d4c547241796b813dbccc845f160188c702bae10
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2018-12-28 10:53:28 -0500

    fixed pictures

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 
 const About = () => (
       <div className='about'>
-        <h1>About Me</h1>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+        <h2>Lester Caballero</h2>
+          <img src="Lester_Caballero02.jpg" alt="Lester" id="lcaballero"
+              height={100}
+              width={100}/>
+        <h2>Denise Pennaman</h2>
+            <img src="Denise_Pennaman.jpg" alt="Denise" id="dpennaman"
+                height={100}
+                width={100}/>
+        <h2>Ana Gonzalez</h2>
+            <img src="Ana_Gonzalez01.jpg" alt="Ana" id="agonzalez"
+                height={100}
+                width={100}/>
+        <h2>Quintin Osuchukwu</h2>
+            <img src="Quintin_Osuchukwu01.jpg" alt="Quintin" id="qosuchukwu"
+                height={100}
+                width={100}/>
       </div>
     );
 
